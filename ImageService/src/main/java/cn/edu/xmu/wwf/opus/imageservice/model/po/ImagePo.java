94e8c0c2eccfbc904e7f3eb290119754e908681b
Wenfei Wang <395110618@qq.com>
2022-05-05 21:53:33 +0800

    2022-05-04: Image模块集成redis

--- ImageService/src/main/java/cn/edu/xmu/wwf/opus/imageservice/model/po/ImagePo.java
+++ ImageService/src/main/java/cn/edu/xmu/wwf/opus/imageservice/model/po/ImagePo.java
@@ -2,8 +2,10 @@ package cn.edu.xmu.wwf.opus.imageservice.model.po;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class ImagePo {
+public class ImagePo implements Serializable {
     int id;
     String name;
     String url;
