871e97769703e7c375d29129a408cbcc788848d8
Wenfei Wang <395110618@qq.com>
2022-05-06 19:50:13 +0800

    2022-05-04: Login的Cookie设置为sameSite:None secure:false

--- UserService/src/main/java/cn/edu/xmu/wwf/opus/userservice/service/UserService.java
+++ UserService/src/main/java/cn/edu/xmu/wwf/opus/userservice/service/UserService.java
@@ -35,9 +35,10 @@ public class UserService {
         }
         String token=generateToken(userPo);
         ResponseCookie cookie = ResponseCookie.from("token",token)
-                        .path("/")
-                                .sameSite("None")
-                                        .build();
+                .path("/")
+                .sameSite("None")
+                .secure(false)
+                .build();
         httpServletResponse.setHeader(HttpHeaders.SET_COOKIE,cookie.toString());
         return new ReturnObject(new UserLoginRetVo(true,token));
     }
