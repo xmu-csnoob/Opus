d3cbd8e355b04a8292fe15d0d2e1809a53460900
xmu-csnoob <395110618@qq.com>
2022-06-04 14:59:21 +0800

    2022-05-04: User模块新增替换头像功能

--- ImageService/src/main/java/cn/edu/xmu/wwf/opus/imageservice/controller/ImageController.java
+++ ImageService/src/main/java/cn/edu/xmu/wwf/opus/imageservice/controller/ImageController.java
@@ -8,11 +8,18 @@ import cn.edu.xmu.wwf.opus.imageservice.model.vo.ImageUrlRetVo;
 import cn.edu.xmu.wwf.opus.imageservice.service.ImageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 @Api(tags = "图片")
 @RestController
@@ -39,5 +46,31 @@ public class ImageController {
     @GetMapping("/{id}")
     public ReturnObject<ImageUrlRetVo> getImageInfo(@PathVariable int id){
         return imageService.getUrlRetVo(id);
+    }
+    @PostMapping("/api")
+    public Object Api(@RequestPart("image")MultipartFile multipartFile) throws IOException {
+
+        File file = null;
+        ITesseract instance = new Tesseract();
+
+        InputStream ins=null;
+        ins=multipartFile.getInputStream();
+        file=new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        FileUtils.copyInputStreamToFile(multipartFile.getInputStream(), file);
+        //设置训练库的位置
+        String lagnguagePath = "C:\\Users\\wangwenfei\\IdeaProjects\\Opus\\ImageService\\src\\main\\resources\\files\\tessdata";
+        instance.setDatapath(lagnguagePath);
+
+        instance.setLanguage("chi_sim");//chi_sim ：简体中文， eng	根据需求选择语言库
+        String result = null;
+        try {
+            long startTime = System.currentTimeMillis();
+            result =  instance.doOCR(file);
+            long endTime = System.currentTimeMillis();
+            System.out.println("Time is：" + (endTime - startTime) + " 毫秒");
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
